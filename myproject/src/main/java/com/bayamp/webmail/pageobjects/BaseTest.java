95f06cbdc0b77f0f424a84e354b99a9a38f483bd
Harish Appannagari <training@bayamp.com>
2022-05-04 16:14:41 -0700

    Rest API Test

--- myproject/src/main/java/com/bayamp/webmail/pageobjects/BaseTest.java
+++ myproject/src/main/java/com/bayamp/webmail/pageobjects/BaseTest.java
@@ -9,31 +9,38 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 	protected WebDriver driver;
+	private Properties prop;
 
 	@BeforeSuite
 	public void loadDriver() throws IOException {
 		
-		InputStream configFile = new FileInputStream("./src/main/resources/config.properties");
-		Properties prop = new Properties();
-		prop.load(configFile);
+		if (prop == null) {
+			InputStream configFile = new FileInputStream("./src/main/resources/config.properties");
+			prop = new Properties();
+			prop.load(configFile);
+		}
 		
 		String browser = prop.getProperty("browserType");
+		Reporter.log("Browser Type is :" + browser,true);
+		
 		String driverLocation = prop.getProperty("driver.location");
+		Reporter.log("Driver Location  is :" + driverLocation,true);
 		
 		if("chrome".equals(browser)) {
-	
+			Reporter.log("Loading chrome driver",true);
 			System.setProperty("webdriver.chrome.driver",driverLocation );
 			
 		}
 		
 		if("firefox".equals(browser)) {
-			
+			Reporter.log("Loading firefox driver",true);
 			System.setProperty("webdriver.gecko.driver",driverLocation );
 			
 		}
--- myproject/src/main/java/com/bayamp/webmail/pageobjects/LoginPage.java
+++ myproject/src/main/java/com/bayamp/webmail/pageobjects/LoginPage.java
@@ -1,10 +1,7 @@
 package com.bayamp.webmail.pageobjects;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage extends Page {
 	
